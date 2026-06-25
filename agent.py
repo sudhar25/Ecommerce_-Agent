9c07107a9d824a44db21ee4995b05e40b25fa080
sudharsan <sudharsannadar1@gmail.com>
2026-06-25 20:25:18 +0530

    Add visualization tools for monthly sales and hourly purchase peaks

--- agent.py
+++ agent.py
@@ -3,9 +3,21 @@ from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain_community.utilities import SQLDatabase
 from langchain_community.agent_toolkits import create_sql_agent
-
+from visualizer import plot_monthly_sales, plot_hourly_peaks
 load_dotenv()
 
+@tool
+def generate_monthly_sales_chart(year: int) -> str:
+    """useful when the user ask to visualize or chat"""
+    path = plot_monthly_sales(year)
+    return f"chart generated successfully and saved to local file: {path}"
+
+@tool
+def generate_hourly_peak_chart() -> str:
+    """useful when the user ask for hourly purchase"""
+    path = plot_hourly_peaks()
+    return f"hourly peak chart generated successfully and saved to local file: {path}"
+    
 if not os.getenv("GROQ_API_KEY"):
     raise ValueError("API Key not found! Check your .env file.")
 
@@ -16,17 +28,17 @@ llm = ChatGroq(
     temperature=0
 )
 
+extra_tools = [generate_monthly_sales_chart, generate_hourly_peak_chart ]
 agent_executor = create_sql_agent(
     llm=llm,
     db=db,
     agent_type="openai-tools",
+    extra_tools=extra_tools,
     verbose=True
 )
 
 if __name__ == "__main__":
-    print("agent is initialized and ready to analyze.")
-    
-    question = "how many columns are in the orders table, and what are their names?"
-    print(f"\nAsking Agent: {question}\n")
+    # Test routing to custom tools
+    question = "Can you show me a chart of our monthly sales performance for the year 2017?"
     response = agent_executor.invoke({"input": question})
-    print(f"\nfinal Answer:\n{response['output']}")
+    print(response['output'])
